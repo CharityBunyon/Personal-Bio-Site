d216717386b24b81397ee337805e17e65e622e4b
Charity Bunyon <cseymou5@gmail.com>
2020-02-15 17:37:28 -0600

    new layout for about and nav section

--- src/javascripts/components/navBar/navbar.js
+++ src/javascripts/components/navBar/navbar.js
@@ -12,11 +12,10 @@ const buildNav = () => {
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
   <div class="collapse navbar-collapse navLinks" id="navbarSupportedContent">
     <ul class="navbar-nav container">
       <li class="nav-item active">
-        <a class="nav-link" id="navLink" href="#about">ABOUT</a>
+        <a class="nav-link" id="navLink" href="#about1">ABOUT</a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" id="navLink" href="#skills">SKILLS</a>
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,8 +5,10 @@ import contact from './components/contact/contact';
 import projectDisplay from './components/projectDisplay/projectDisplay';
 import skills from './components/skills/skills';
 import welcome from './components/welcome/welcome';
+import about from './components/about/about';
 
 const init = () => {
+  about.buildAbout();
   navBar.buildNav();
   contact.buildContact();
   skills.buildSkills();
